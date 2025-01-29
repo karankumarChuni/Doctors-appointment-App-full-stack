a976942dd6cc744a2ae045f2e4a9cbba80133cc0
karankumaryadav <kkcgamer865@gmail.com>
2025-01-29 23:06:50 +0530

    add typescript files and add dipendencies

--- frontend-new/src/main.tsx
+++ frontend-new/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>,
 )
