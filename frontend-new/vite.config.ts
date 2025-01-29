68d2ce1accfa69d2b2958b599bb244cb5be7c713
karankumaryadav <kkcgamer865@gmail.com>
2025-01-30 00:00:13 +0530

    vite change

--- frontend-new/vite.config.ts
+++ frontend-new/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3498db' // Add your primary color hex code
+      }
+    },
+  },
+  plugins: [react(), tailwindcss()],
 })
